3a32ba1f137875ce83c3e55c8b776c663a568cf5
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-31 16:14:33 +0300

    add userEdit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.User;
 import com.example.demo.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +15,15 @@ public class UserController {
     @Autowired
     private UserRepo userRepo;
     @GetMapping
-    public String userList(){
+    public String userList(Model model){
+        model.addAttribute("users",userRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model){
+        model.addAttribute("user",user);
+        return "userEdit";
     }
 
 }
