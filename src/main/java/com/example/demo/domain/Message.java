7ef71f636c9f66846529d2766a70eefdc6d4586d
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-31 11:25:33 +0300

    update

--- src/main/java/com/example/demo/domain/Message.java
+++ src/main/java/com/example/demo/domain/Message.java
@@ -16,9 +16,14 @@ public class Message {
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author=user;
+    }
+
+    public String getAuthorName(){
+        return author!=null ? author.getUsername():"<none>";
     }
 
     public Integer getId() {
