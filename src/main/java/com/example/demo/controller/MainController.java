ff2c92c3c992155a7ac5ffc8ad6a4fb611910e9d
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-31 14:01:49 +0300

    update

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -22,9 +22,15 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(Map<String,Object> model){
+    public String main(@RequestParam(required = false) String filter, Map<String,Object> model){
         Iterable<Message> messages = messageRepo.findAll();
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages",messages);
+        model.put("filter",filter);
         return "main";
     }
 
@@ -38,19 +44,7 @@ public class MainController {
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
-
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
-        return "main";
-    }
 }
