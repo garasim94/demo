6a33eb170e5bae22ce7f417f43b931a7b390d5d7
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-31 15:04:25 +0300

    add method userList

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+    @GetMapping
+    public String userList(){
+        return "userList";
+    }
+
 }
