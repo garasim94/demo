4775afe9c9bb46c188dc618fa369511b7d4bd832
veronikakruchok <kruchok.veronika@gmail.com>
2023-03-31 14:36:56 +0300

    update

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.demo.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,15 +23,15 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(@RequestParam(required = false) String filter, Map<String,Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
         if (filter != null && !filter.isEmpty()) {
             messages = messageRepo.findByTag(filter);
         } else {
             messages = messageRepo.findAll();
         }
-        model.put("messages",messages);
-        model.put("filter",filter);
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter",filter);
         return "main";
     }
 
